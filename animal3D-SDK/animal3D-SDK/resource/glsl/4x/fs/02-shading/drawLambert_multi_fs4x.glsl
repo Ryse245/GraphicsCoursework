c1b952eac3f37ca15670c0afa29299a94b6192f8
Ben Hubner <ben.hubner@mymail.champlain.edu>
2020-01-27 15:58:03 -0500

    Achieved :)

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawLambert_multi_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawLambert_multi_fs4x.glsl
@@ -30,11 +30,12 @@ uniform sampler2D uTex_dm;
 //	2) declare uniform variables for lights; see demo code for hints
 //CORRECT
 uniform int uLightCt;
-uniform vec4 uLightCol;
 //INCORRECT
 uniform float uLightSz;
 uniform float uLightSzInvSq;
-uniform vec3 uLightPos;
+//CORRECT
+uniform vec4 uLightPos[4];
+uniform vec4 uLightCol[4];
 
 //	3) declare inbound varying data
 in vec2 vTextureCoord;
@@ -44,23 +45,24 @@ in vec4 viewPos;
 //	Note: test all data and inbound values before using them!
 
 out vec4 rtFragColor;
-
+int test;
+/*
 float lambertize()
 {
 	vec3 fragNormal = normalize(vecNormal);
-	vec3 lightNormal = normalize(uLightPos);
+	vec4 lightNormal = normalize(uLightPos);
 	float lambProduct = max(0.0, dot(fragNormal, lightNormal));
 	return lambProduct;
 }
+*/
 
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE RED
-	//rtFragColor = vec4(vTextureCoord, 0,0);
 	//rtFragColor = vec4(vecNormal,0);
-	//rtFragColor = uLightCol;
-	//rtFragColor = vec4(abs(uLightCt), 0,0,0);
-
+	//rtFragColor = uLightPos[2];
+	//rtFragColor = vec4(uLightPos[1],1);
 	//rtFragColor = lambertize() * uLightCol * texture(uTex_dm, vTextureCoord);
+
 	rtFragColor = texture(uTex_dm, vTextureCoord);
 }
