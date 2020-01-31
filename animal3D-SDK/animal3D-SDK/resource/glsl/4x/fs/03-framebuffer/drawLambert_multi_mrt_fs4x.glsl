bd08bd26e914d994e2756fec1486e5ea33b5d532
Stephen <stephen.hill@mymail.champlain.edu>
2020-01-31 12:16:41 -0500

    Worked on Lab 3 :)

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/03-framebuffer/drawLambert_multi_mrt_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/03-framebuffer/drawLambert_multi_mrt_fs4x.glsl
@@ -24,19 +24,53 @@
 
 #version 410
 
+////https://learnopengl.com/Lighting/Basic-Lighting helped with finding out what to do
+
 // ****TO-DO: 
 //	1) declare uniform variable for texture; see demo code for hints
+uniform sampler2D uTex_dm;
 //	2) declare uniform variables for lights; see demo code for hints
+
+uniform int uLightCt;
+
+uniform vec4 uLightPos[4];
+uniform vec4 uLightCol[4];
+
 //	3) declare inbound varying data
+in vec2 vTexcoord;
+in vec4 vecNormal;
+in vec4 viewPos;
 //	4) implement Lambert shading model
 //	Note: test all data and inbound values before using them!
-//	5) set location of final color render target (location 0)
-//	6) declare render targets for each attribute and shading component
 
-out vec4 rtFragColor;
+//	5) set location of final color render target (location 0)
+layout(location = 0) out vec4 rtFragColor;
+
+//	6) declare render targets for each attribute and shading component
+out vec4 vTexcoord;
+out vec4 rtVecNormal;
+out vec4 rtViewPos;
+
+float lambertize(vec4 V, vec4 L, vec4 F)
+{
+	vec4 fragNormal = normalize(V);
+	vec4 lightNormal = normalize(L-F);
+	float lambProduct = max(0.0, dot(fragNormal, lightNormal));
+	return lambProduct;
+}
+
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE RED
-	rtFragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	vec4 finalLightCol;
+
+	for(int i = 0; i < uLightCt; i++)
+	{
+		finalLightCol +=  lambertize(vecNormal, uLightPos[i], viewPos) * uLightCol[i];
+	}
+
+	rtFragColor = finalLightCol * texture(uTex_dm, vTextureCoord);
+
 }
+
+
