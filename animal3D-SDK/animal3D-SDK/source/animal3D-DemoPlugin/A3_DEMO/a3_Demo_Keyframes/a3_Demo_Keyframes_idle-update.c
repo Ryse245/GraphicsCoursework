3fb198d9ad0dad1efa97f79c518186535dca7160
Stephen Hill <stephen.hill@mymail.champlain.edu>
2020-04-08 16:04:00 -0400

    Added another pose. idle-input line 126 change 0 to 1 for second pose, as well as in idle-render line 179. idle-update line 159 change 1 to 0 to display first pose

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Keyframes/a3_Demo_Keyframes_idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Keyframes/a3_Demo_Keyframes_idle-update.c
@@ -156,7 +156,7 @@ void a3keyframes_update(a3_DemoState* demoState, a3_Demo_Keyframes* demoMode, a3
 	currentHierarchy = currentHierarchyPoseGroup->hierarchy;
 
 	a3hierarchyPoseCopy(currentHierarchyState->localPose,
-		currentHierarchyPoseGroup->pose + 0, currentHierarchy->numNodes);
+		currentHierarchyPoseGroup->pose + 1, currentHierarchy->numNodes);
 	a3hierarchyPoseConvert(currentHierarchyState->localSpace,
 		currentHierarchyState->localPose, currentHierarchy->numNodes, 0);
 	a3kinematicsSolveForward(demoState->hierarchyState_skel);
